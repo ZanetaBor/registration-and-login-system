8675aab4f8e49a68e38ab3e7d460b7a0556d9b3b
Żaneta Bortnik <zanweb.developer@gmail.com>
2025-01-30 15:04:35 +0100

    Update gra.php

--- gra.php
+++ gra.php
@@ -13,7 +13,9 @@
 <html lang="pl">
 <head>
 	<meta charset="utf-8"/>
-	<meta https-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> 
+	<meta http-equiv="Content-Security-Policy" content="default-src *;
+   		img-src * 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *;
+   		style-src  'self' 'unsafe-inline' *"> 
 	<tile>Sim kot - gra przeglądarkowa</title>
 	<link rel="stylesheet" href="style2.css" type="text/css"/>
 </head>
