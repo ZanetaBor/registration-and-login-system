4a6090245489014c574c9993b3c3d541916f075f
Żaneta Bortnik <zanweb.developer@gmail.com>
2025-01-30 15:07:26 +0100

    Update witamy.php

--- witamy.php
+++ witamy.php
@@ -30,7 +30,9 @@
 <html lang="pl">
 <head>
 	<meta charset="utf-8"/>
-	<meta https-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> 
+	<meta http-equiv="Content-Security-Policy" content="default-src *;
+   		img-src * 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *;
+   		style-src  'self' 'unsafe-inline' *">
 	<title>Sim Kot - gra przeglądarkowa</title>
 	<link rel="stylesheet" href="style2.css" type="text/css"/>
 </head>
